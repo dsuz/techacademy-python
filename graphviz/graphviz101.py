5fba0479802a3a6d91393cc49a160b0397c84f2e
bboydaisuke <bboydaisuke@gmail.com>
2018-03-03 15:15:01 +0900

    update for matplotlib/graphviz

---
+++ graphviz/graphviz101.py
@@ -0,0 +1,23 @@
+from graphviz import Digraph
+import datetime
+
+# formatはpngを指定(他にはPDF, PNG, SVGなどが指定可)
+G = Digraph(format='png')
+G.attr('node', shape='circle')
+
+N = 15    # ノード数
+
+# ノードの追加
+for i in range(N):
+    G.node(str(i), str(i))
+
+# 辺の追加
+for i in range(N):
+    if (i - 1) // 2 >= 0:
+        G.edge(str((i - 1) // 2), str(i))
+
+# print()するとdot形式で出力される
+print(G)
+
+# binary_tree.pngで保存
+G.render('binary_tree' + '{0:%Y%m%d%H%M%S}'.format(datetime.datetime.now()))
